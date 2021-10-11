ea03bdab9bb682b804dd4b2fa7f3906f0328c6ad
shoji360 <shoji@shoji-lab.jp>
2021-10-11 22:57:27 +0900

    change for_mod_0

--- for_mod_0.c
+++ for_mod_0.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.i>
 
 void main(){
     // very slow prime number finder
     int num;
     printf("input your favorite number: ");
     scanf("%d", &num);
-    for (int i = 2; i <= num; i++){
+    for (int i = 1; i <= num; i++){
         if (i == num){
             printf("%d is prime number!", num);
             break;
         }
-        if (num % i == 0){
+        if (i % num == 0){
             printf("%d can be divided by %d", num, i);
             break;
         }

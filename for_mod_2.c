ec8b00009d67cea5832096c474b468b218f83cae
syou126 <s1911474@s.tsukuba.ac.jp>
2021-10-14 04:30:53 +0900

    fix for_mod_2.c

--- for_mod_2.c
+++ for_mod_2.c
@@ -3,12 +3,13 @@
 void main(void){
     int i = 100;
     while(1){
-        if(i = 0){
+        if(i == 0){
+            printf("0 comes, break \n");
             break;
-            printf("0 comes, break")
+        }else{
+            printf("please input the number (exit if 0 comes) \n");
+            scanf("%d", &i);
+            printf("your input is %d \n", i);
         }
-        printf("please input the number (exit if 0 comes)");
-        scanf("%d", &i);
-        printf("your input is %f", i);
     }
 }

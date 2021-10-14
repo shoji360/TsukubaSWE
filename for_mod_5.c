925ecdd30a8bd05e591d107208b9496b78e433ac
tagaihahimazin <tagainao@icloud.com>
2021-10-14 16:04:27 +0900

    fix ¥n

--- for_mod_5.c
+++ for_mod_5.c
@@ -4,7 +4,7 @@ int main(void){
     int price;
     printf("input price:");
     scanf("%d", &price);
-    printf("tax include: %f yen", 1.1 * price);
+    printf("tax include: %f yen\n", 1.1 * price);
 
     return 0;
 }

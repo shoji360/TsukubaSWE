8e88e0a4a3bd542ab2828aacf5fef3c4c7758ff1
tagaihahimazin <tagainao@icloud.com>
2021-10-14 15:29:42 +0900

    fix bug for_mod_5.c

--- for_mod_5.c
+++ for_mod_5.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-    int price
-    printf("input price:")
-    scanf("%d", &price)
-    printf("tax include: %f yen", 1.1 * price)
+int main(void){
+    int price;
+    printf("input price:");
+    scanf("%d", &price);
+    printf("tax include: %f yen", 1.1 * price);
+
+    return 0;
+}
 
 

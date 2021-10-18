5a9571bec5eea78cf3a3f60d08e0e8d840c5cb5d
k-usuha <kouta528bado@yahoo.co.jp>
2021-10-18 16:16:55 +0900

    add new line for_mod_1.c

--- for_mod_1.c
+++ for_mod_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void main(){
-    printf("hello world!!!");
+    printf("hello world!!!\n");
     // this line is a comment!!
     for (int i = 1; i < 10; i++){
         printf("i is %d\n", i);

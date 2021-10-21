14384be9606d468bb5e23341d02fc78af122e659
ippo <s1911477@s.tsukuba.ac.jp>
2021-10-22 06:47:55 +0900

    fix for_mod_5.c

--- for_mod_5.c
+++ for_mod_5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-    int price
-    printf("input price:")
-    scanf("%d", &price)
-    printf("tax include: %f yen", 1.1 * price)
-
-
+int main(void){
+    int price;
+    printf("input price:");
+    scanf("%d", &price);
+    printf("tax include: %f yen", 1.1 * price);
+}

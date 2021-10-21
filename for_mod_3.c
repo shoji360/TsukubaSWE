4cd8a9266180eb4df6b34cf45cf851df41ebc917
jima0906 <s1911445@s.tsukuba.ac.jp>
2021-10-22 01:49:03 +0900

    fix for_mod_3.c

--- for_mod_3.c
+++ for_mod_3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    char str[9] = "123456789";
-    printf("dont input longer than 9 letters:");
-    scanf("%s", &str);
+    char str[9];
+    printf("don't input longer than 9 letters:");
+    scanf("%s", str);
     printf("%s", str);
 }
